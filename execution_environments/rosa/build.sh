c619638c6a23e6c7e1045c50b64330f554d75bc1
matt <l3acon@gmail.com>
2025-08-25 19:11:09 -0600

    no way this works

---
+++ execution_environments/rosa/build.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+if [[ -z $ANSIBLE_GALAXY_SERVER_CERTIFIED_TOKEN || -z $ANSIBLE_GALAXY_SERVER_VALIDATED_TOKEN ]]
+then
+    echo "A valid Automation Hub token is required, Set the following environment variables before continuing"
+    echo "export ANSIBLE_GALAXY_SERVER_CERTIFIED_TOKEN=<token>"
+    echo "export ANSIBLE_GALAXY_SERVER_VALIDATED_TOKEN=<token>"
+    exit 1
+fi
+
+# log in to pull the base EE image
+if ! podman login --get-login registry.redhat.io > /dev/null
+then
+    echo "Run 'podman login registry.redhat.io' before continuing"
+    exit 1
+fi
+
+# create EE definition
+rm -rf ./context/*
+ansible-builder create \
+    --file execution-environment.yml \
+    --context ./context \
+    -v 3 | tee ansible-builder.log
+
+# remove existing manifest if present
+_tag=$(date +%Y%m%d)
+podman manifest rm quay.io/matferna/madhatter:${_tag}
+
+# create manifest for EE image
+podman manifest create quay.io/matferna/madhatter:${_tag}
+
+# for the openshift-clients RPM, microdnf doesn't support URL-based installs
+# and HTTP doesn't support file globs for GETs, use multiple steps to determine
+# the correct RPM URL for each machine architecture
+for arch in amd64 arm64
+do
+    _baseurl=https://mirror.openshift.com/pub/openshift-v4/${arch}/dependencies/rpms/4.18-el9-beta/
+    _rpm=$(curl -s ${_baseurl} | grep openshift-clients-4 | grep href | cut -d\" -f2)
+
+    # build EE for multiple architectures from the EE context
+    pushd ./context/ > /dev/null
+    podman build --platform linux/${arch} \
+      --build-arg ANSIBLE_GALAXY_SERVER_CERTIFIED_TOKEN \
+      --build-arg ANSIBLE_GALAXY_SERVER_VALIDATED_TOKEN \
+      --build-arg OPENSHIFT_CLIENT_RPM="${_baseurl}${_rpm}" \
+      --manifest quay.io/matferna/madhatter:${_tag} . \
+      | tee podman-build-${arch}.log
+    popd > /dev/null
+done
+
+echo "Built quay.io/matferna/madhatter:${_tag}"
+
+# inspect manifest content
+#podman manifest inspect quay.io/matferna/madhatter:${_tag}
+
+# tag manifest as latest
+#podman tag quay.io/matferna/madhatter:${_tag} quay.io/matferna/madhatter:latest
+
+# push all manifest content to repository
+# using --all is important here, it pushes all content and not
+# just the native platform content
+#podman manifest push --all quay.io/matferna/madhatter:${_tag}
+#podman manifest push --all quay.io/matferna/madhatter:latest
